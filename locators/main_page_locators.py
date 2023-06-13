a59b1f4076bdebe2830ba8ba612cfc2b3676e623
Christina Lebedeva <qa.christina.lebedeva@yandex.ru>
2023-06-13 20:47:56 +0200

    Modified locators

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -2,10 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    COOKIE_ACCEPT_BUTTON_LOCATOR = (By.CLASS_NAME, "App_CookieButton__3cvqF")
-    MOST_ASKED_QUESTIONS_SECTION_HEADING_LOCATOR = (By.CLASS_NAME, "Home_FourPart__1uthg")
-    QUESTION_LOCATOR = (By.XPATH, "//div[@class='accordion__item' ]")
+    COOKIE_ACCEPT_BUTTON_LOCATOR = (By.XPATH, "//button[contains(@class, 'App_CookieButton')]")
+    MOST_ASKED_QUESTIONS_SECTION_HEADING_LOCATOR = (By.XPATH, "//div[contains(@class, 'Home_FAQ')]")
+    QUESTION_LOCATOR = (By.XPATH, "//div[contains(@class, 'accordion__item')]")
     ANSWER_LOCATOR = (By.XPATH, "//div[contains(@class, 'accordion__panel') and not(@hidden)]")
-    TOP_ORDER_BUTTON_LOCATOR = (By.CLASS_NAME, "Button_Button__ra12g")
-    BOTTOM_ORDER_BUTTON_LOCATOR = (By.XPATH, "//button[@class='Button_Button__ra12g Button_UltraBig__UU3Lp']")
+    TOP_ORDER_BUTTON_LOCATOR = (By.XPATH, "//div[contains(@class, 'Header_Nav')]/button[contains(@class, "
+                                          "'Button_Button__ra12g')]")
+    BOTTOM_ORDER_BUTTON_LOCATOR = (By.XPATH, "//div[contains(@class, 'Home_FinishButton')]/button[contains(@class, "
+                                             "'Button_Button__ra12g')]")
     DZEN_LOGO_LOCATOR = (By.XPATH, "//a[@aria-label='Логотип Дзен']")
--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -8,21 +8,24 @@ class OrderPageLocators:
     STATION_LOCATOR = (By.XPATH, "//input[@placeholder='* Станция метро']")
     REQUIRED_STATION_LOCATOR = (By.XPATH, "//ul/li/button[@value='103']")
     PHONE_NUMBER_LOCATOR = (By.XPATH, "//input[@placeholder='* Телефон: на него позвонит курьер']")
-    NEXT_BUTTON = (By.XPATH, "//button[@class='Button_Button__ra12g Button_Middle__1CSJM']")
+    NEXT_BUTTON = (By.XPATH, "//div[contains(@class, 'Order_NextButton')]/button[contains(@class, "
+                             "'Button_Button__ra12g')]")
     RENTAL_DATE_LOCATOR = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']")
     NEXT_DAY_LOCATOR = (By.XPATH, "//div[contains(@class, 'day--today')]/following-sibling::div")
     EXCLUDED_DATE_LOCATOR = (By.XPATH, "//div[contains(@aria-label,'воскресенье')]")
     REPLACED_DATE_LOCATOR = (By.XPATH, "//div[contains(@aria-label,'понедельник')]")
-    RENTAL_PERIOD_LOCATOR = (By.XPATH, "//div[@class='Dropdown-control']")
+    RENTAL_PERIOD_LOCATOR = (By.XPATH, "//div[contains(@class, 'Dropdown-control')]")
     ONE_DAY_LOCATOR = (By.XPATH, "//div[contains(text(), 'сутки')]")
     TWO_DAYS_LOCATOR = (By.XPATH, "//div[contains(text(), 'двое суток')]")
     BLACK_COLOR_LOCATOR = (By.ID, "black")
     GREY_COLOR_LOCATOR = (By.ID, "grey")
-    COMMENT_LOCATOR = (By.XPATH, "//input[@class='Input_Input__1iN_Z Input_Responsible__1jDKN']")
-    ORDER_BUTTON_LOCATOR = (By.XPATH, "//button[@class='Button_Button__ra12g Button_Middle__1CSJM']")
+    COMMENT_LOCATOR = (By.XPATH, "//div[contains(@class, 'Input_InputContainer')]/input[contains(@class, "
+                                 "'Input_Input__1iN_Z Input_Responsible__1jDKN')]")
+    ORDER_BUTTON_LOCATOR = (By.XPATH, "//div[contains(@class, 'Order_Buttons')]/button[@class='Button_Button__ra12g "
+                                      "Button_Middle__1CSJM']")
     ORDER_CONFIRMATION_BUTTON_LOCATOR = (By.XPATH, "//button[contains(text(),'Да')]")
     CHECK_ORDER_STATUS_LOCATOR = (By.XPATH, "//button[contains(text(), 'Посмотреть статус')]")
-    ORDER_NUMBER_INPUT_LOCATOR = (By.XPATH, ".//input[@class='Input_Input__1iN_Z Track_Input__1g7lq "
-                                            "Input_Filled__1rDxs Input_Responsible__1jDKN']")
-    SCOOTER_LOGO_LOCATOR = (By.CLASS_NAME, "Header_LogoScooter__3lsAR")
-    YANDEX_LOGO_LOCATOR = (By.CLASS_NAME, "Header_LogoYandex__3TSOI")
+    ORDER_NUMBER_INPUT_LOCATOR = (By.XPATH, "//div[contains(@class, 'Input_InputContainer')]/input[contains(@class, "
+                                            "'Input_Input__1iN_Z Track_Input')]")
+    SCOOTER_LOGO_LOCATOR = (By.XPATH, "//a[contains(@class, 'Header_LogoScooter')]")
+    YANDEX_LOGO_LOCATOR = (By.XPATH, "//a[contains(@class, 'Header_LogoYandex')]")
