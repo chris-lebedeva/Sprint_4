0aa03abc6db8a2bf2bc7af2dbcf06aa2bdffe678
Christina Lebedeva <qa.christina.lebedeva@yandex.ru>
2023-06-14 21:38:48 +0200

    Added test classes names

--- tests/test_create_order_success.py
+++ tests/test_create_order_success.py
@@ -5,40 +5,41 @@ from page_objects.main_page import MainPage
 from page_objects.order_page import OrderPage
 
 
-@allure.title('Проверка оформления заказа через кнопку "Заказать" в правом верхнем углу главной страницы')
-@allure.description('Нажимаем на кнопку "Заказать" в правом верхнем углу страницы и проходим весь положительный флоу '
-                    'оформления заказа с возвратом на главную страницу по клику на логотип "Самокат"')
-def test_create_order_by_clicking_on_the_top_button(driver):
-    main_page = MainPage(driver)
-    main_page.go_to_site()
-    main_page.accept_cookies()
-    main_page.click_on_top_order_button()
-    order_page = OrderPage(driver)
-    order_page.fill_in_client_info()
-    order_page.fill_in_order_details(OrderPage.RentalPeriod.ONE_DAY, OrderPage.Color.BLACK, Comments.first_comment)
-    order_page.finish_order_creation()
-    order_number = order_page.get_order_number()
-    assert order_number in driver.current_url
-    order_page.click_on_scooter_logo()
-    assert main_page.current_url() == Urls.SCOOTER_MAIN_PAGE_URL
+class TestOrderCreation:
 
+    @allure.title('Проверка оформления заказа через кнопку "Заказать" в правом верхнем углу главной страницы')
+    @allure.description('Нажимаем на кнопку "Заказать" в правом верхнем углу страницы и проходим весь положительный '
+                        'флоу оформления заказа')
+    def test_create_order_by_clicking_on_the_top_button(self, driver):
+        main_page = MainPage(driver)
+        main_page.go_to_site()
+        main_page.accept_cookies()
+        main_page.click_on_top_order_button()
+        order_page = OrderPage(driver)
+        order_page.fill_in_client_info()
+        order_page.fill_in_order_details(OrderPage.RentalPeriod.ONE_DAY, OrderPage.Color.BLACK, Comments.first_comment)
+        order_page.finish_order_creation()
+        order_number = order_page.get_order_number()
+        assert order_number in driver.current_url
+        order_page.click_on_scooter_logo()
+        assert main_page.current_url() == Urls.SCOOTER_MAIN_PAGE_URL
 
-@allure.title('Проверка оформления заказа через кнопку "Заказать" в нижней части главной страницы')
-@allure.description('Нажимаем на кнопку "Заказать" в нижней части главной страницы и проходим весь положительный флоу '
-                    'оформления заказа с переходом на страницу "Дзен" по клику на логотип "Яндекс"')
-def test_create_order_by_clicking_on_the_bottom_button(driver):
-    main_page = MainPage(driver)
-    main_page.go_to_site()
-    main_page.accept_cookies()
-    main_page.scroll_to_the_bottom_order_button()
-    main_page.click_on_bottom_order_button()
-    order_page = OrderPage(driver)
-    order_page.fill_in_client_info()
-    order_page.fill_in_order_details(OrderPage.RentalPeriod.TWO_DAYS, OrderPage.Color.GREY, Comments.second_comment)
-    order_page.finish_order_creation()
-    order_number = order_page.get_order_number()
-    assert order_number in driver.current_url
-    order_page.click_on_yandex_logo()
-    driver.switch_to.window(driver.window_handles[1])
-    main_page.wait_for_dzen_logo()
-    assert main_page.current_url() == Urls.DZEN_MAIN_PAGE_URL
+    @allure.title('Проверка оформления заказа через кнопку "Заказать" в нижней части главной страницы')
+    @allure.description('Нажимаем на кнопку "Заказать" в нижней части главной страницы и проходим весь положительный '
+                        'флоу оформления заказа')
+    def test_create_order_by_clicking_on_the_bottom_button(self, driver):
+        main_page = MainPage(driver)
+        main_page.go_to_site()
+        main_page.accept_cookies()
+        main_page.scroll_to_the_bottom_order_button()
+        main_page.click_on_bottom_order_button()
+        order_page = OrderPage(driver)
+        order_page.fill_in_client_info()
+        order_page.fill_in_order_details(OrderPage.RentalPeriod.TWO_DAYS, OrderPage.Color.GREY, Comments.second_comment)
+        order_page.finish_order_creation()
+        order_number = order_page.get_order_number()
+        assert order_number in driver.current_url
+        order_page.click_on_yandex_logo()
+        driver.switch_to.window(driver.window_handles[1])
+        main_page.wait_for_dzen_logo()
+        assert main_page.current_url() == Urls.DZEN_MAIN_PAGE_URL
--- tests/test_most_asked_questions_on_main_page.py
+++ tests/test_most_asked_questions_on_main_page.py
@@ -4,15 +4,17 @@ from page_objects.main_page import MainPage
 from data.data import Answers
 
 
-@allure.title('Проверка списка часто задаваемых вопросов и ответов на главной странице')
-@allure.description('На главной странице ищем список часто задаваемых вопросов и ответов и проверяем корректность '
+class TestQuestionsAndAnswers:
+
+    @allure.title('Проверка списка часто задаваемых вопросов и ответов на главной странице')
+    @allure.description('На главной странице ищем список часто задаваемых вопросов и ответов и проверяем корректность '
                     'ответов')
-@pytest.mark.parametrize("index", range(8))
-def test_most_asked_questions_on_main_page(driver, index):
-    main_page = MainPage(driver)
-    main_page.go_to_site()
-    main_page.accept_cookies()
-    main_page.scroll_to_the_most_asked_questions_section()
-    main_page.click_on_questions(index)
-    answer = main_page.get_answers()
-    assert answer == Answers.answers[index], f'Получен некорректный текст ответа. Ожидалось: {answer}'
+    @pytest.mark.parametrize("index", range(8))
+    def test_most_asked_questions_on_main_page(self, driver, index):
+        main_page = MainPage(driver)
+        main_page.go_to_site()
+        main_page.accept_cookies()
+        main_page.scroll_to_the_most_asked_questions_section()
+        main_page.click_on_questions(index)
+        answer = main_page.get_answers()
+        assert answer == Answers.answers[index], f'Получен некорректный текст ответа. Ожидалось: {answer}'
